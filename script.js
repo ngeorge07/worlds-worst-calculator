d29e0d718f26b711b08423c4bb89e380de9a56e7
George <geor2335@stud.kea.dk>
2022-02-06 17:05:59 +0100

    added operators working

--- script.js
+++ script.js
@@ -8,5 +8,23 @@ calcBtn.addEventListener("click", calculate);
 function calculate() {
   const firstNr = parseFloat(document.querySelector("#firstnumber").value);
   const secondNr = parseFloat(document.querySelector("#secondnumber").value);
-  console.log(firstNr + secondNr);
+
+  const operator = operatorList.options[operatorList.selectedIndex].text;
+
+  const calc = {
+    "+": function (a, b) {
+      return a + b;
+    },
+    "-": function (a, b) {
+      return a - b;
+    },
+    "*": function (a, b) {
+      return a * b;
+    },
+    "/": function (a, b) {
+      return a / b;
+    },
+  };
+
+  console.log(calc[operator](firstNr, secondNr));
 }
